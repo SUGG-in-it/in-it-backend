ed7b414d9a249a4b540a0868065aef3f07dde5cd
hellojihyoung <jihyoungkwon@gmail.com>
2022-07-19 22:09:02 +0900

    project init

--- src/test/java/com/example/initbackend/InitBackendApplicationTests.java
+++ src/test/java/com/example/initbackend/InitBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.init;
+package com.example.initbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InitApplicationTests {
+class InitBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
