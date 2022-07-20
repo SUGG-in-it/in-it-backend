58c73d332ac413e4cfbc6d6b8aa014935d934e18
hellojihyoung <jihyoungkwon@gmail.com>
2022-07-20 23:18:01 +0900

    :sparkles: UserToekn 테이블 스키마 생성

---
+++ src/main/java/com/example/initbackend/domain/UserToken.java
@@ -0,0 +1,27 @@
+package com.example.initbackend.domain;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "user_token")
+public class UserToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private Integer user_id;
+
+    @Column(nullable = false)
+    private String refresh_token;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    @CreationTimestamp
+    private Timestamp updateDate;
+}
