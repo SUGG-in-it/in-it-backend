a8bbb48a05714ebc727ec5af910805ef0711b53a
ssuh0o0 <ssuh0o0@daum.net>
2022-07-22 00:46:08 +0900

    :sparkles: created controller directory

--- src/main/java/com/example/initbackend/InitBackendApplication.java
+++ src/main/java/com/example/initbackend/InitBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InItBackendApplication {
+public class InitBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InItBackendApplication.class, args);
+		SpringApplication.run(InitBackendApplication.class, args);
 	}
 
 }
