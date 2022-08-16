7444197624e5f3b1d496d9c281fb8d2bda41dd12
soo <ssuh0o0@daum.net>
2022-08-16 21:55:57 +0900

    :passport_control: add cors setting

--- src/main/java/com/example/initbackend/InItBackendApplication.java
+++ src/main/java/com/example/initbackend/InItBackendApplication.java
@@ -2,12 +2,19 @@ package com.example.initbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class InItBackendApplication {
+public class InItBackendApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(InItBackendApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*");
 	}
 
 }
