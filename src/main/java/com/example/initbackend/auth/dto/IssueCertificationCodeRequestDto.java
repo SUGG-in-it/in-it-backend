3898f401128243ce60868f7b23a5e8b33271f0c0
hellojihyoung <jihyoungkwon@gmail.com>
2022-08-16 23:29:37 +0900

    :sparkles: add email verification api

---
+++ src/main/java/com/example/initbackend/auth/dto/IssueCertificationCodeRequestDto.java
@@ -0,0 +1,26 @@
+package com.example.initbackend.auth.dto;
+
+import com.example.initbackend.auth.domain.Auth;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class IssueCertificationCodeRequestDto {
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "이메일 양식 오류")
+    private String email;
+
+    public IssueCertificationCodeRequestDto(String email) {
+        this.email = email;
+    }
+    public Auth toEntity(String certificationCode, Integer userId){
+        return Auth.builder()
+                .user_id(userId)
+                .code(certificationCode)
+                .build();
+    }
+}
