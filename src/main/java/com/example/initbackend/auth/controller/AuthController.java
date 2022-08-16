3dd82d2a63793273d99691635e85c44d62237cf6
hellojihyoung <jihyoungkwon@gmail.com>
2022-08-16 23:52:36 +0900

    :sparkles: AuthController.java 추가

---
+++ src/main/java/com/example/initbackend/auth/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.example.initbackend.auth.controller;
+
+import com.example.initbackend.auth.dto.IssueCertificationCodeRequestDto;
+import com.example.initbackend.auth.service.AuthService;
+import com.example.initbackend.global.response.StatusEnum;
+import com.example.initbackend.global.response.SuccessResponse;
+import com.example.initbackend.user.dto.LoginRequestDto;
+import com.example.initbackend.user.service.UserService;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Getter
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+    private final AuthService authService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping({ "/issue" })
+    public SuccessResponse issueCertificationCode(@Valid @RequestBody final IssueCertificationCodeRequestDto requestDto) {
+        authService.issueCertificationCode(requestDto);
+        SuccessResponse res = SuccessResponse.builder()
+                .status(StatusEnum.OK)
+                .message("issued certification code")
+                .build();
+
+        return res;
+    }
+}
