0bd812060c967e0a11a0a36664a7c6f2150a32c3
hellojihyoung <jihyoungkwon@gmail.com>
2022-09-04 21:06:05 +0900

    :sparkles: question vo directory 생성

--- src/main/java/com/example/initbackend/question/controller/QuestionController.java
+++ src/main/java/com/example/initbackend/question/controller/QuestionController.java
@@ -3,7 +3,7 @@ package com.example.initbackend.question.controller;
 import com.example.initbackend.global.response.StatusEnum;
 import com.example.initbackend.global.response.SuccessResponse;
 import com.example.initbackend.question.service.QuestionService;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.example.initbackend.question.vo.IssueQuestionIdResponseVo;
 import lombok.Getter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,15 +24,7 @@ public class QuestionController {
         // token 까서 userid 넘겨주기
         Long questionId = questionService.issueQuestionId(12);
 
-        class IssueQuestionIdResponse{
-            @JsonProperty
-            private Long questionId;
-            public IssueQuestionIdResponse(Long questionId) {
-                this.questionId = questionId;
-            }
-        }
-
-        IssueQuestionIdResponse issueQuestionIdResponse = new IssueQuestionIdResponse(questionId);
+        IssueQuestionIdResponseVo issueQuestionIdResponse = new IssueQuestionIdResponseVo(questionId);
 
         SuccessResponse res = SuccessResponse.builder()
                 .status(StatusEnum.OK)
