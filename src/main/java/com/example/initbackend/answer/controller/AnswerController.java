e853b57696f41a07586c70615a4a7a6d907b8fb4
soo <ssuh0o0@daum.net>
2022-09-13 22:47:18 +0900

    :sparkles: GetAnswer 생성

--- src/main/java/com/example/initbackend/answer/controller/AnswerController.java
+++ src/main/java/com/example/initbackend/answer/controller/AnswerController.java
@@ -1,21 +1,24 @@
 package com.example.initbackend.answer.controller;
 
 
+import com.example.initbackend.answer.domain.Answer;
 import com.example.initbackend.answer.dto.CreateAnswerRequestDto;
+import com.example.initbackend.answer.dto.GetAnswerRequestDto;
 import com.example.initbackend.answer.service.AnswerService;
+import com.example.initbackend.answer.vo.GetAnswerResponseVo;
 import com.example.initbackend.global.response.StatusEnum;
 import com.example.initbackend.global.response.SuccessResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import org.springframework.data.domain.Pageable;
 
-@Component
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/answers")
@@ -30,6 +33,17 @@ public class AnswerController {
 
         SuccessResponse res = SuccessResponse.builder()
                 .status(StatusEnum.OK)
+                .build();
+        return res;
+    }
+
+    @GetMapping
+    public SuccessResponse getAnswer(HttpServletRequest request, Pageable pageable){
+        GetAnswerResponseVo getAnswerResponseVo = answerService.getAnswer(request, pageable);
+
+        SuccessResponse res = SuccessResponse.builder()
+                .status(StatusEnum.OK)
+                .data(getAnswerResponseVo)
                 .build();
         return res;
     }
