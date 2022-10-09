05eaa1198f19f213193a547cc03af4396599dfcd
ssuh0o0 <ssuh0o0@daum.net>
2022-10-10 00:10:05 +0900

    :recycle: add tagList type

--- src/main/java/com/example/initbackend/tag/service/TagService.java
+++ src/main/java/com/example/initbackend/tag/service/TagService.java
@@ -1,7 +1,11 @@
 package com.example.initbackend.tag.service;
 
 
+import com.example.initbackend.global.handler.CustomException;
+import com.example.initbackend.global.response.ErrorCode;
+import com.example.initbackend.questionTag.repository.QuestionTagRepository;
 import com.example.initbackend.tag.domain.Tag;
+import com.example.initbackend.tag.dto.GetTagListRequestDto;
 import com.example.initbackend.tag.repository.TagRepository;
 import com.example.initbackend.tag.vo.GetTagListResponseVo;
 import lombok.Getter;
@@ -19,14 +23,34 @@ import java.util.List;
 public class TagService {
 
     private final TagRepository tagRepository;
+    private final QuestionTagRepository questionTagRepository;
 
-    public GetTagListResponseVo getTagList(){
-
-        List<Tag> optionalTag = tagRepository.findAllByOrderByTag();
+    public GetTagListResponseVo getTagList(GetTagListRequestDto getTagListRequestDto){
+        String type = getTagListRequestDto.getType();
         List<String> tags = new ArrayList<>();
-        for(Tag t :optionalTag){
-            tags.add(t.getTag());
+
+        if (type.equals("All")){
+            List<Tag> optionalTag = tagRepository.findAllByOrderByTag();
+
+            for(Tag t :optionalTag){
+                tags.add(t.getTag());
+            }
+
+        } else if (type.equals("popular")){
+            List<Long> popularTagIds = questionTagRepository.countTagIdByOrderByCount();
+            System.out.println(popularTagIds);
+            List<Tag> popularTags = tagRepository.findTop10ByIdIn(popularTagIds);
+            System.out.println(popularTags);
+            for(Tag t : popularTags){
+                tags.add(t.getTag());
+            }
+
+            System.out.println(tags);
+
+        } else {
+            throw new CustomException(ErrorCode.TAG_TYPE_ERROR);
         }
+
         GetTagListResponseVo getTagListResponseVo = new GetTagListResponseVo(tags);
         return getTagListResponseVo;
     }
