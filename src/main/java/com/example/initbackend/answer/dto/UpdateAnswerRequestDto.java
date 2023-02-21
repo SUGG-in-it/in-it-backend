6ab8904c19e2e5e2504efdde222a21cb10de9c97
soo <ssuh0o0@daum.net>
2023-02-22 03:04:12 +0900

    :white_check_mark: answer test 추가

--- src/main/java/com/example/initbackend/answer/dto/UpdateAnswerRequestDto.java
+++ src/main/java/com/example/initbackend/answer/dto/UpdateAnswerRequestDto.java
@@ -2,14 +2,18 @@ package com.example.initbackend.answer.dto;
 
 import com.example.initbackend.answer.domain.Answer;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class UpdateAnswerRequestDto {
 
     private String content;
 
+    @Builder
+    public UpdateAnswerRequestDto(String content) {
+        this.content = content;
+    }
 }
